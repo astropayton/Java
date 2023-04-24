5d0a6b2d37b3fd45e4da1978adc7ea3247d63c42
astropayton <126277325+astropayton@users.noreply.github.com>
2023-04-24 16:18:58 -0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,22 @@
+// create a object with object keys in it and values 
+
+let information = {
+    firstName: ("jake"),
+    lastName: ("smith"),
+    email: ("jaekSmih!aol.com"),
+    phone: (undefined),
+    zipcode: ("631"),
+    favoriteFlavors: (32),
+    cupSize: ("medium"),
+    favoriteStore: ("Target"),
+    firstVisit: (false)
+}
+// Use bracket notation to update some incorrect info
+
+let email = []; {
+    information.email = ("Jak3Smith1992@email.com");
+    information.phone = ("319-555-1234");
+    information.zipcode = ("63132");
+    information.favoriteFlavors = ("coffee"), ("strawberry"), ("matcha")
+}
+console.log(information);
